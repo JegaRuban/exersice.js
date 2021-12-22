e500a606e78ed4ea03fd5b47bbd397214631361d
JegaRuban <96118620+JegaRuban@users.noreply.github.com>
2021-12-22 13:35:28 +0530

    printing fr8 owner code for 100 times

---
+++ exe2.js
@@ -0,0 +1,4 @@
+const myName="We are frugal. We value our money, time & resources and ensure they are deployed in the most productive way";
+for(let i = 0; i <= 100; i++){
+    console.log(myName);
+}
